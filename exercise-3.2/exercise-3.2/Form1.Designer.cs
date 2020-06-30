429096caf7622a3b93be21b4b3f377d226cc1d69
Elias Bengtsson <elias.bengtsson@thelindata.se>
2020-06-30 15:22:38 +0200

    Summerize two numbers

---
+++ exercise-3.2/exercise-3.2/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace exercise_3._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(378, 221);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(276, 110);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(87, 17);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "First number";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(264, 153);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(108, 17);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "Second number";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(375, 187);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(48, 17);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "Result";
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(378, 110);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(100, 22);
+            this.txtBox1.TabIndex = 4;
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(378, 153);
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(100, 22);
+            this.txtBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnRun);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.TextBox txtBox2;
+    }
+}
+
---
+++ exercise-3.2/exercise-3.2/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exercise_3._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            double firstNumber = double.Parse(txtBox1.Text);
+            double secoundNumber = double.Parse(txtBox2.Text);
+            double result = firstNumber + secoundNumber;
+            lblResult.Text = result.ToString();
+        }
+    }
+}
