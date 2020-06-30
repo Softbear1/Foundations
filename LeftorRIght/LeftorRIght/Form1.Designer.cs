f986956ea51a44ce14481190a92bf752df128427
Elias Bengtsson <elias.bengtsson@thelindata.se>
2020-06-30 11:42:49 +0200

    Finished left or right exercise

---
+++ LeftorRIght/LeftorRIght/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace LeftorRIght
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.lblFlytta = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(190, 179);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(75, 60);
+            this.btnLeft.TabIndex = 0;
+            this.btnLeft.Text = "Left";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(475, 179);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(75, 60);
+            this.btnRight.TabIndex = 1;
+            this.btnRight.Text = "Right";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // lblFlytta
+            // 
+            this.lblFlytta.AutoSize = true;
+            this.lblFlytta.Location = new System.Drawing.Point(298, 353);
+            this.lblFlytta.Name = "lblFlytta";
+            this.lblFlytta.Size = new System.Drawing.Size(81, 17);
+            this.lblFlytta.TabIndex = 2;
+            this.lblFlytta.Text = "Left or right";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblFlytta);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Label lblFlytta;
+    }
+}
+
---
+++ LeftorRIght/LeftorRIght/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeftorRIght
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Left button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblFlytta.Left = lblFlytta.Left - 10;
+        }
+        
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            lblFlytta.Left = lblFlytta.Right + 10;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
