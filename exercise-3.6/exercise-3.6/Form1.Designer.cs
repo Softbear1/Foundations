578daf8bedf2e43527ccc0391c04a2285d2609ae
Elias Bengtsson <elias.bengtsson@thelindata.se>
2020-07-02 08:56:16 +0200

    Visar initialerna på för och efternamn

---
+++ exercise-3.6/exercise-3.6/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace exercise_3._6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxFörnamn = new System.Windows.Forms.TextBox();
+            this.tbxEfternamn = new System.Windows.Forms.TextBox();
+            this.lblFörnamn = new System.Windows.Forms.Label();
+            this.lblEfternamn = new System.Windows.Forms.Label();
+            this.lblSvar = new System.Windows.Forms.Label();
+            this.btnKor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxFörnamn
+            // 
+            this.tbxFörnamn.Location = new System.Drawing.Point(415, 104);
+            this.tbxFörnamn.Name = "tbxFörnamn";
+            this.tbxFörnamn.Size = new System.Drawing.Size(100, 20);
+            this.tbxFörnamn.TabIndex = 0;
+            // 
+            // tbxEfternamn
+            // 
+            this.tbxEfternamn.Location = new System.Drawing.Point(415, 146);
+            this.tbxEfternamn.Name = "tbxEfternamn";
+            this.tbxEfternamn.Size = new System.Drawing.Size(100, 20);
+            this.tbxEfternamn.TabIndex = 1;
+            // 
+            // lblFörnamn
+            // 
+            this.lblFörnamn.AutoSize = true;
+            this.lblFörnamn.Location = new System.Drawing.Point(348, 104);
+            this.lblFörnamn.Name = "lblFörnamn";
+            this.lblFörnamn.Size = new System.Drawing.Size(57, 15);
+            this.lblFörnamn.TabIndex = 2;
+            this.lblFörnamn.Text = "Förnamn";
+            // 
+            // lblEfternamn
+            // 
+            this.lblEfternamn.AutoSize = true;
+            this.lblEfternamn.Location = new System.Drawing.Point(348, 146);
+            this.lblEfternamn.Name = "lblEfternamn";
+            this.lblEfternamn.Size = new System.Drawing.Size(64, 15);
+            this.lblEfternamn.TabIndex = 3;
+            this.lblEfternamn.Text = "Efternamn";
+            // 
+            // lblSvar
+            // 
+            this.lblSvar.AutoSize = true;
+            this.lblSvar.Location = new System.Drawing.Point(412, 184);
+            this.lblSvar.Name = "lblSvar";
+            this.lblSvar.Size = new System.Drawing.Size(0, 15);
+            this.lblSvar.TabIndex = 4;
+            // 
+            // btnKor
+            // 
+            this.btnKor.Location = new System.Drawing.Point(415, 216);
+            this.btnKor.Name = "btnKor";
+            this.btnKor.Size = new System.Drawing.Size(75, 23);
+            this.btnKor.TabIndex = 5;
+            this.btnKor.Text = "Kör";
+            this.btnKor.UseVisualStyleBackColor = true;
+            this.btnKor.Click += new System.EventHandler(this.btnKor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKor);
+            this.Controls.Add(this.lblSvar);
+            this.Controls.Add(this.lblEfternamn);
+            this.Controls.Add(this.lblFörnamn);
+            this.Controls.Add(this.tbxEfternamn);
+            this.Controls.Add(this.tbxFörnamn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxFörnamn;
+        private System.Windows.Forms.TextBox tbxEfternamn;
+        private System.Windows.Forms.Label lblFörnamn;
+        private System.Windows.Forms.Label lblEfternamn;
+        private System.Windows.Forms.Label lblSvar;
+        private System.Windows.Forms.Button btnKor;
+    }
+}
+
---
+++ exercise-3.6/exercise-3.6/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exercise_3._6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKor_Click(object sender, EventArgs e)
+        {
+            string förnamn = tbxFörnamn.Text;
+            string efternamn = tbxEfternamn.Text;
+            char första = förnamn[0];
+            char andra = efternamn[0];
+            string svar = förnamn + " " + efternamn + " har initialerna " + första + ". " + andra + ".";
+            lblSvar.Text = svar;
+        }
+    }
+}
